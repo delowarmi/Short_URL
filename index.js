3b77e244f092f10659d96bcd1edef3a5ad9d6d32
Muhammad Delowar Hosen MI <md48735002@gmail.com>
2025-02-07 23:03:32 +0600

    add 404 error page and improve server startup message

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+
 import { app } from "./src/app.js";
 import { APP_URL, PORT } from "./src/constants.js";
 app.listen(PORT,()=>{
-        console.log(`server is running${APP_URL}:${PORT}`);
+        console.log(`server is running on ${APP_URL}:${PORT}`);
         
 })
--- src/app.js
+++ src/app.js
@@ -1,8 +1,16 @@
 import e from "express";
 const app = e()
+import path from "path";
+import { fileURLToPath } from "url";
 app.use (e.json({limit:'1mb'}))
 app.use(e.urlencoded({limit:'1mb',extended:true}))
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 app.get('/',(_,res)=>{
-        res.send("server in runnung")
+        res.send("server is runnung")
+})
+app.all('*',(_,res)=>{
+        res.status(404).sendFile(path.join(__dirname,"views","404.html"))
 })
 export{app}
